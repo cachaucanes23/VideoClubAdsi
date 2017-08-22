0c21c2ea28151624bcd1e400b944b7aa7fb7d70b
carlos chaucanes <cachaucanes23@gmail.com>
2017-08-22 09:24:59 -0500

    proyecto video club

---
+++ client/app/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('videoClubApp', [
+    'videoClubApp.constants', 
+    'ngCookies', 
+    'ngResource', 
+    'ngSanitize',
+    'ui.router', 
+    'satellizer',
+    'ngMaterial'
+  ])
+  .constant("API","http://localhost:8080/adsi2017_backend_completo")
+  .config(function($urlRouterProvider, $locationProvider,$authProvider,API) {
+    $urlRouterProvider.otherwise('/');
+    $locationProvider.html5Mode(true);
+    // Config Satellizer
+      $authProvider.loginUrl = API+'/api/auth/login';
+      $authProvider.tokenName = 'token';
+      $authProvider.tokenPrefix = 'VideoClubAdsi';
+  });
